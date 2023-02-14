9d465df03033c9f6c4febefe1a4e7feaf8736b61
AneeqMalik <aneeqfayyazkarimmalik@gmail.com>
2023-02-15 00:28:16 +0500

    Changed the incorrect sockect address from send_message to send_msg

--- Server/index.js
+++ Server/index.js
@@ -1,42 +1,38 @@
-const express=require("express");
-const app=express();
+const express = require("express");
+const app = express();
 
-const http=require("http");
-const cors=require("cors");
-const { Server }=require("socket.io");
-
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
 
 app.use(cors());
-const server=http.createServer(app);
-const io= new Server(server,{
- cors:{
- origin:"http://localhost:3000",
- method:["GET","POST"],
- },
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    method: ["GET", "POST"],
+  },
 });
-io.on("connection",(socket)=>{
- console.log(`User Connected : ${socket.id}`);
- socket.on("join_room",(data)=>{
-  socket.join(data);
-  console.log(`User joined with the user id ${socket.id} and Room id ${data}`);
+io.on("connection", (socket) => {
+  console.log(`User Connected : ${socket.id}`);
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(
+      `User joined with the user id ${socket.id} and Room id ${data}`
+    );
+  });
 
+  //send_msg 
+  socket.on("send_msg", (data) => {
+    socket.to(data.Room).emit("receive_message", data);
 
- });
- socket.on("send_msg",(data)=>{
-  socket.to(data.Room).emit("receive_message",data);
+    // socket.to(data.Room).emit("receive_msg",data);
+  });
 
-
-  // socket.to(data.Room).emit("receive_msg",data); 
- });
-
- socket.on("disconnect",(socket)=>{
-  console.log("User disconnected",socket.id);
-
- });
-
-})
-server.listen(3002 ,()=>{
- console.log("Running successfully");
-
+  socket.on("disconnect", (socket) => {
+    console.log("User disconnected", socket.id);
+  });
 });
-
+server.listen(3002, () => {
+  console.log("Running successfully");
+});
--- client/my-app/src/Chat.js
+++ client/my-app/src/Chat.js
@@ -1,65 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-export default function Chat({socket,Username,Room}) {
-  const [message,setMessage]=useState("");
- // const [messageList,setmessageList]=useState([]);
-
-  const sendmessage= async ()=>{
-    if(message!==""){
-      const MessageData={
-        Room:Room,
-        author:Username,
-        msg:message,
-        
-        time: new Date(Date.now()).getHours() + ":" +
-        new Date(Date.now()).getMinutes() + ":" +   new Date(Date.now()).getSeconds(),
-
-
-      }
-      await socket.emit("send_msg",MessageData);
-     // setmessageList((list)=>[...list,MessageData]);
-
-
-    }
-  };
- 
-  
-  // useEffect(()=>{
-  //   socket.on("receive_msg",(data)=>{
-  //     console.log(data);
-
-  //     //setmessageList((list)=>[...list,data])  
-
-  //   });
-
-  // },[socket]);
+export default function Chat({ socket, Username, Room }) {
+  const [message, setMessage] = useState("");
+  const [messageList, setmessageList] = useState([]);
   useEffect(() => {
     socket.on("receive_message", (data) => {
-     console.log(data);
-     
+      // console.log(data);
+      setmessageList((list) => [...list, data]);
     });
   }, [socket]);
+  const sendmessage = async () => {
+    if (message !== "") {
+      const MessageData = {
+        Room: Room,
+        author: Username,
+        msg: message,
+
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes() +
+          ":" +
+          new Date(Date.now()).getSeconds(),
+      };
+
+      //send_msg was written as send_message
+      await socket.emit("send_msg", MessageData);
+      setmessageList((list) => [...list, MessageData]);
+      setMessage("");
+    }
+  };
 
   return (
-    <div className='chat-window'>
-      <div className='chat-header'>Chat Live</div>
-      <div className='chat-body'>
-
-      
+    <div className="chat-window">
+      <div className="chat-header">Chat Live</div>
+      <div className="chat-body">
+        {messageList.map((messageContent) => {
+          return <p>{messageContent.msg}</p>;
+        })}
       </div>
-      <div className='chat-footer'>
-        <input placeholder='hey...' type="text" 
-        onChange={(event)=>{
-          setMessage(event.target.value);
-
-        }}
-        
-        
-        
+      <div className="chat-footer">
+        <input
+          value={message}
+          placeholder="hey..."
+          type="text"
+          onChange={(event) => {
+            setMessage(event.target.value);
+          }}
+          onKeyDown={(event) => {
+            event.key === "Enter" && sendmessage();
+          }}
         />
         <button onClick={sendmessage}>Sent</button>
       </div>
     </div>
-  )
+  );
 }
